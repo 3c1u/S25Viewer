36b8341426267a355f0a73cb511aaf4b503ccd33
Hikaru Terazono (3c1u) <3c1u@vulpesgames.tokyo>
2020-05-21 20:54:42 +0900

    initial commit

---
+++ s25imageview.cpp
@@ -0,0 +1,6 @@
+#include "s25imageview.h"
+
+S25ImageView::S25ImageView()
+{
+
+}
---
+++ s25imageview.h
@@ -0,0 +1,12 @@
+#ifndef S25IMAGEVIEW_H
+#define S25IMAGEVIEW_H
+
+
+class S25ImageView : public QOpenGLWidget
+{
+    Q_OBJECT
+public:
+    S25ImageView();
+};
+
+#endif // S25IMAGEVIEW_H
